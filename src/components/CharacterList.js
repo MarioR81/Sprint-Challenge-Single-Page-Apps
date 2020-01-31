4ae2d3e26aad974e72bc32bf6580815929951e40
apple <apple@apples-MacBook-Pro-2.local>
2020-01-31 10:00:55 -0800

    finished axios call

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,41 @@
 import React, { useEffect, useState } from "react";
+import axios from 'axios';
+import { useParams } from 'react-router-dom';
+import CharacterCard from './CharacterCard';
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
-
+const [character, setCharacter] = useState();
+const {id} = useParams();
   useEffect(() => {
     // TODO: Add API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
-  }, []);
+    axios
+    .get(`https://cors-anywhere.herokuapp.com/https://rickandmortyapi.com/api/character/`)
+    .then(res => {
+      // console.log('axios'. res);
+      setCharacter(res.data.results);
+    }) 
+    .catch(error => {
+      console.log(error);
+    });
+  }, [id]);
 
-  return (
-    <section className="character-list">
-      <h2>TODO: `array.map()` over your state here!</h2>
-    </section>
-  );
-}
+  // console.log('response', character);
+
+  if (!character) {
+    return <div>Loading character information...</div>;
+  }
+
+ 
+    return (
+        <div>
+            
+                {character.map((character) => {
+                    console.log('map', character);
+                    return <CharacterCard character={character} />
+                })}
+                
+                
+          </div>
+)}
